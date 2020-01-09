db774480c935d39cfe54934b1d0edff3ea3df826
kuckjwi <skyblue0928@datajunta.io>
2020-01-09 09:54:19 +0900

    Modified ugly logic.

--- server/crawling/views.py
+++ server/crawling/views.py
@@ -12,42 +12,45 @@ from requests import get
 def is_possible_crawling(req: HttpRequest) -> JsonResponse:
     # TODO(kuckjwi): subdomain scan.
     # TODO(kuckjwi): url valid check.
-    if url := req.GET['url']:
-        robot = get(f'{url}/robots.txt')
+    url = req.GET.get('url')
 
-        if robot.status_code == status.HTTP_404_NOT_FOUND:
-            return JsonResponse({
-                'message': 'Robots file not found',
-                'content': '',
-            })
-
-        parser = RobotParser()
-        parser.parse(robot.content.decode('utf-8'))
-
-        if not('*' in parser.get_allow_urls()):
-            return JsonResponse({
-                'message': 'Not allowed url',
-                'content': '',
-            })
-
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        options.add_argument("disable-gpu")
-
-        with webdriver.Chrome(CHROME_DRIVER_PATH, chrome_options=options) as driver:
-            driver.implicitly_wait(3)
-            driver.get(url)
-            soup = BeautifulSoup(driver.page_source, 'lxml')
-            body_tag = soup.find('body')
-            script_tag = body_tag.find_all('script')
-            for tag in script_tag:
-                tag.extract()
+    if not url:
         return JsonResponse({
-            'message': 'Success',
-            'content': body_tag.prettify(),
+            'message': 'Missing url parameter.',
+            'content': '',
         })
 
+    robot = get(f'{url}/robots.txt')
+
+    if robot.status_code == status.HTTP_404_NOT_FOUND:
+        return JsonResponse({
+            'message': 'Robots file not found',
+            'content':  '',
+        })
+
+    parser = RobotParser()
+    parser.parse(robot.content.decode('utf-8'))
+
+    if not('*' in parser.get_allow_urls()):
+        return JsonResponse({
+            'message': 'Not allow crawling',
+            'content': '',
+        })
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument("disable-gpu")
+
+    with webdriver.Chrome(CHROME_DRIVER_PATH, chrome_options=options) as driver:
+        driver.implicitly_wait(3)
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        body_tag = soup.find('body')
+        script_tag = body_tag.find_all('script')
+        for tag in script_tag:
+            tag.extract()
+
     return JsonResponse({
-        'message': 'Missing url parameter.',
-        'content': '',
+        'message': 'Success',
+        'content': body_tag.prettify(),
     })
